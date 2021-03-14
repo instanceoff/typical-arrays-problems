c4a66ecda5562ba84d1f5e3f2602a10e8114e69b
Ilya Khinevich <ilyaxinevich@gmail.com>
2021-03-14 15:19:31 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
   if (!array || array.length === 0){
     return 0;
-   }
+  }
   let sum = 0;
   for (let i = 0; i<array.length; i++){
     sum = sum + array[i];
